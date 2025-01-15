43b6ca3d1a9342d43214d15cb42a05f99e641e15
aurelien-cuvelier <aurelien.cuvelier@gmail.com>
2025-01-15 11:35:44 +0000

    env safety

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,20 @@ import { globalLogger } from "./logger";
 
 export const IS_PRODUCTION = process.env.NODE_ENV !== "production";
 
+/**
+ * This file is the only file where process.env shoud be used. Each env variable should have a getter function here
+ * that will exit the process if it is required for normal work. This prevents deployment to production if the
+ * container's env is not up to date.
+ */
+
+(() => {
+  if (IS_PRODUCTION) {
+    //env var not needed for dev
+  }
+
+  getDatabaseUrl();
+})();
+
 export function getDatabaseUrl(): string {
   const val = process.env.DATABASE_URL;
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import "./config";
+import { IS_PRODUCTION } from "./config";
 import "./logger";
 import { globalLogger } from "./logger";
 
-globalLogger.info(`New backend instance starting`);
+globalLogger.info(
+  `New backend instance starting [IS_PRODUCTION: ${IS_PRODUCTION}]`
+);
