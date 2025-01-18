267ba5af65a0b1e559df500fd3beac0c38011ba5
aurelien-cuvelier <aurelien.cuvelier@gmail.com>
2025-01-18 18:46:48 +0000

    create user test

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,4 +1,6 @@
 import supertest from "supertest";
+import { Web3 } from "web3";
+const determStringify = require("fast-json-stable-stringify");
 
 const PORT = 4000;
 
@@ -10,4 +12,27 @@ test("Get ingredients list", async () => {
   expect(Array.isArray(response.body)).toBe(true);
 
   expect(response.body.length).toBeGreaterThan(500);
+});
+
+test("Create a new user", async () => {
+  const provider = new Web3();
+
+  const account = provider.eth.accounts.create();
+
+  const payload = {
+    username: `TEST_${Date.now()}`,
+    address: account.address,
+    country: "USA",
+  };
+
+  const hashedPayload = provider.utils.sha3(determStringify(payload)) as string;
+  const sig = account.sign(hashedPayload);
+
+  const response = await app
+    .post("/users/create")
+    .set("x-wallet-signature", sig.signature)
+    .send(payload);
+
+  expect(response.statusCode).toBe(200);
+  expect(JSON.parse(response.text)).toHaveProperty("id");
 });
